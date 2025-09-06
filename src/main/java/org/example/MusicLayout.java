ec96c699b580be87af8f990d256d262f0d2054e0
awabqais <awab.qais@gmail.com>
2025-09-06 10:05:39 +0300

    commit#2
    in the NV on line 10,set the layout of the frame

--- src/main/java/org/example/MusicLayout.java
+++ src/main/java/org/example/MusicLayout.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MusicLayout {
     public MusicLayout() {
         JFrame MusicLayoutFrame=new JFrame("Music PlayList");
         JFrame.setDefaultLookAndFeelDecorated(true);
+        MusicLayoutFrame.setLayout(new BorderLayout());
     }
 }
