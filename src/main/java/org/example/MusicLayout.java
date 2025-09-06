37ab91a12d5818a7a793bc542805cf30a82538ef
awabqais <awab.qais@gmail.com>
2025-09-06 09:51:01 +0300

    commit#2
    on the NV on line 7 and 8,create create a JFrame and customize

--- src/main/java/org/example/MusicLayout.java
+++ src/main/java/org/example/MusicLayout.java
@@ -1,6 +1,10 @@
 package org.example;
 
+import javax.swing.*;
+
 public class MusicLayout {
     public MusicLayout() {
+        JFrame MusicLayoutFrame=new JFrame("Music PlayList");
+        JFrame.setDefaultLookAndFeelDecorated(true);
     }
 }
